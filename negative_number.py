38bcfedcc8d05356dd57178e2f66e4be402209b8
gayathri <gayathrikottisala@gmail.com>
2021-07-01 17:43:47 +0530

    this is my second work

---
+++ negative_number.py
@@ -0,0 +1,6 @@
+integer = 20
+print('make negative of 20 is:', -(abs(integer)))
+integer= -20
+print('make negative of -20 is:', -(abs(integer)))
+integer=0
+print('make negative of 0 is:', (abs(integer)))
