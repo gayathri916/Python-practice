f2c9e99582588df9d335fbf053f67ef40950d7e3
gayathri <gayathrikottisala@gmail.com>
2021-07-01 14:29:59 +0530

    this is my third work

---
+++ triangle.py
@@ -0,0 +1,25 @@
+def valid(a,b,c):
+    if a+b>=c and b+c>=a and c+a>=b:
+        return True
+    else:
+        return False
+
+# Function definition for type
+def type_of_triangle(a,b,c):
+    if a==b and b==c:
+        print('Triangle is Equilateral.')
+    elif a==b or b==c or a==c:
+        print('Triangle is Isosceles.')
+    else:
+        print('Triangle is Scalane')
+
+
+a = float(input('Enter length of side a: '))
+b = float(input('Enter length of side b: '))
+c = float(input('Enter length of side c: '))
+
+
+if valid(a, b, c):
+    type_of_triangle(a, b, c)
+else:
+    print('Traingle is Invalid Tringle')
